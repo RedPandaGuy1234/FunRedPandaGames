8e8a8b22e9539dfb0648ec55a775d44d8ceb2383
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-09-16 16:44:44 -0700

    fix: Update URL for valid words in wordle.py

--- mainfile/Wordle/wordle.py
+++ mainfile/Wordle/wordle.py
@@ -6,7 +6,7 @@ import random
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.NotOpenSSLWarning)
 
-txt_url = "https://raw.githubusercontent.com/RedPandaGuy1234/Games/refs/heads/main/production_stuff/Wordle/Valid_Words.txt"
+txt_url = "https://raw.githubusercontent.com/RedPandaGuy1234/Games/refs/heads/main/docs/Valid_Words.txt"
 response = requests.get(txt_url)
 
 if response.status_code == 200:
